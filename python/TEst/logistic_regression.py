f5029226e28cf2b45afcfaa135e6f9b1c5b97303
prabanch <prabanch@gmail.com>
2017-03-16 21:22:48 +0530

    lr updated & working

--- python/TEst/logistic_regression.py
+++ python/TEst/logistic_regression.py
@@ -12,11 +12,23 @@ survived = titanic.columns[1:2]
 # change male to 1 and female to 0
 titanic["Sex"] = titanic["Sex"].apply(lambda sex: 1 if sex == "male" else 0)
 cols = ["Pclass", "Sex", "Age"]
+
+
 data_train = titanic[cols]
+
+titanic[cols].plot.hist()
 data_dependent = titanic[survived]
 model = LogisticRegression()
 model.fit(data_train, titanic[survived].values.ravel())
 print(model)
-predicted = model.predict(data_train)
+print(model.score(data_train, titanic[survived].values.ravel()))
+titanic[cols].plot.hist()
+titanic_test = pd.read_csv('test.csv')
+# change male to 1 and female to 0
+titanic_test["Sex"] = titanic_test["Sex"].apply(lambda sex: 1 if sex == "male" else 0)
+titanic_test = titanic_test.fillna(0)
+data_test = titanic_test[cols]
+predicted = model.predict(data_test)
+print(predicted)
 #predicted = model.predict(data_test)
 
